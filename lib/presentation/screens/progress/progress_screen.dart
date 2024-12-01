e6d36da1797fcc93da9ce08f9fbc757b60cf74b3
Eduir Brazon <eduirbrazon@gmail.com>
2024-12-01 09:55:29 -0400

    fix progress indicator

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -58,36 +58,38 @@ class _ControlledIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
-          return (value / 2) / 100;
-        }).takeWhile((value) => value < 100),
-        builder: (context, snapshot) {
-          final progresiveValue = snapshot.data ?? 0;
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(
-                      value: progresiveValue,
-                      strokeWidth: 2,
+    return StreamBuilder<int>(
+      stream: Stream.periodic(const Duration(milliseconds: 100), (value) {
+        return value + 1; // Incrementa el valor en 1
+      }).take(100), // Emite valores del 0 al 100
+      builder: (context, snapshot) {
+        final progresiveValue = snapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(
+                    value: progresiveValue / 100, // Convertir a porcentaje
+                    strokeWidth: 2,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: LinearProgressIndicator(
+                      value: progresiveValue / 100, // Convertir a porcentaje
                     ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Expanded(
-                        child: LinearProgressIndicator(
-                      value: progresiveValue,
-                    ))
-                  ],
-                ),
-                Text('${(progresiveValue * 100)}')
-              ],
-            ),
-          );
-        });
+                  ),
+                ],
+              ),
+              Text('$progresiveValue'), // Mostrar el valor actual
+            ],
+          ),
+        );
+      },
+    );
   }
 }
