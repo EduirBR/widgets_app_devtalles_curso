845ca7962f341c21dd45c87780be25036ab8e2f3
Eduir Brazon <eduirbrazon@gmail.com>
2024-12-11 08:29:49 -0400

    seccion 10 finished

--- lib/presentation/screens/inifinite/inifinite_scroll_screen.dart
+++ lib/presentation/screens/inifinite/inifinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -34,6 +35,7 @@ class _InfiniteScroolScreenState extends State<InfiniteScroolScreen> {
     setState(() {
       isLoading = false;
     });
+    moveScrollToBottom();
   }
 
   @override
@@ -53,6 +55,30 @@ class _InfiniteScroolScreenState extends State<InfiniteScroolScreen> {
     super.dispose();
   }
 
+  Future<void> onRefresh() async {
+    setState(() {
+      isLoading = true;
+    });
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void moveScrollToBottom() {
+    if (scrollController.position.pixels + 150 <=
+        scrollController.position.maxScrollExtent) return;
+    scrollController.animateTo(scrollController.position.pixels + 120,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.fastOutSlowIn);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,23 +89,30 @@ class _InfiniteScroolScreenState extends State<InfiniteScroolScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-            controller: scrollController,
-            itemCount: imagesIds.length,
-            itemBuilder: (context, index) {
-              return FadeInImage(
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: 300,
-                  placeholder:
-                      const AssetImage('assets/images/jar-loading.gif'),
-                  image: NetworkImage(
-                      'https://picsum.photos/id/${imagesIds[index]}/500/300'));
-            }),
+        child: RefreshIndicator(
+          edgeOffset: 10,
+          onRefresh: onRefresh,
+          child: ListView.builder(
+              controller: scrollController,
+              itemCount: imagesIds.length,
+              itemBuilder: (context, index) {
+                return FadeInImage(
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 300,
+                    placeholder:
+                        const AssetImage('assets/images/jar-loading.gif'),
+                    image: NetworkImage(
+                        'https://picsum.photos/id/${imagesIds[index]}/500/300'));
+              }),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back_ios),
+        child: !isLoading
+            ? const Icon(Icons.arrow_back_ios_new)
+            : SpinPerfect(
+                infinite: true, child: const Icon(Icons.arrow_back_ios_new)),
       ),
     );
   }
